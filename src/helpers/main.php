ec13038ec530d947c34c27598d8d037d12d27856
Muhammad Yousaf <77118786+yousafitpro@users.noreply.github.com>
2023-05-13 09:54:13 +0500

    Update main.php

--- src/helpers/main.php
+++ src/helpers/main.php
@@ -1,37 +1,3 @@
 <?php
 
 
-if (! function_exists('uc_lib_get_widget')) {
-    function  uc_lib_get_widget($user_id=0) {
-        ///asdasdsdasdas
-       return view('UcPhotoLib::widget',['user_id'=>$user_id]);
-    }
-}
-
-if (! function_exists('uc_lib_get_modal')) {
-    function  uc_lib_get_modal($user_id) {
-        ///asdasdsdasdasadasdasd
-       return view('UcPhotoLib::gallery_modal',['user_id'=>$user_id]);
-    }
-}
-if ( ! function_exists('app_encrypt')){
-    function app_encrypt($number)
-    {
-        return ($number * 91378264);
-    }
-}
-if ( ! function_exists('app_decrypt')){
-    function app_decrypt($number)
-    {
-        return ($number / 91378264);
-    }
-}
-function saveImageAjax($img, $path, $name = null)
-{
-    $name = $name ?: rand(1000, 9999) . time() . '.' . $img->getClientOriginalExtension();
-    $img->storeAs($path,$name,'public');
-
-   // $file = $img->file->store('public/documents');
-    return $name;
-}
-//asdasd
